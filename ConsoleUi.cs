3b3c28b347f85f3d357f8776eb2f4a02b8168739
LenaGrassauer <lenagrassauer@gmail.com>
2025-09-14 13:21:43 +0200

    Update ConsoleUi.cs

--- ConsoleUi.cs
+++ ConsoleUi.cs
@@ -1,16 +1,22 @@
 using haggling_ui.Views;
 
+
 namespace haggling_ui
 {
     public interface IUi
     {
-        void ShowProducts(IEnumerable<Product> products, Vendor vendor, Consumer consumer);
-        void ShowOffer(Offer offer, Vendor vendor, Consumer consumer);
+        void ShowProducts(IEnumerable<Product> products, Vendor vendor, Customer customer);
+        void ShowOffer(Offer offer, Vendor vendor, Customer customer);
     }
 
     public class HagglingUI : IUi
     {
-        public void ShowProducts(IEnumerable<Product> products, Vendor vendor, Consumer consumer)
+        public void ShowOffer(Offer offer, Vendor vendor, Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowProducts(IEnumerable<Product> products, Vendor vendor, Customer customer)
         {
 
         }
